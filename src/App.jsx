58ee329254c01e97fccf51a1ed4354fe48ed69f3
Raylightgr <g.sotiriou@hotmail.com>
2023-08-17 21:22:34 +0300

    Searchbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-
 import "./App.css";
 import Searchbar from "./components/Searchbar";
 import CardList from "./components/CardList";
+import FetchRecipe from "./components/FetchRecipe";
 
 function App() {
   return (
     <>
       <Searchbar />
       <CardList />
+      <FetchRecipe />
     </>
   );
 }
