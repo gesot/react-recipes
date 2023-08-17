bc9e857826a71148a4fcca75a545c8b9b77cfe8b
Raylightgr <g.sotiriou@hotmail.com>
2023-08-17 21:22:34 +0300

    Added Fetch component

---
+++ src/components/FetchRecipe.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function FetchRecipe() {
+  const [imageSrc, setImageSrc] = useState(null);
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php"
+      );
+      const resData = await response.json();
+      console.log(resData);
+      console.log(resData.meals[0].strMealThumb);
+      setImageSrc(resData.meals[0].strMealThumb);
+    }
+    // fetchRecipes();
+  }, []);
+  return <>{imageSrc && <img src={imageSrc} alt="Fetched Img" />}</>;
+}
